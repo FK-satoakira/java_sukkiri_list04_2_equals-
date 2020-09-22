6456f5f42b88ea5de0c6822c640c630fecba82c9
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-22 19:06:55 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,33 +1,69 @@
 package practice;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
+class Monster{
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean equals(Object o) {
+		if(this==o)return true;
+		if(o==null)return false;
+		if(!(o instanceof Monster))return false;
+		Monster h = (Monster) o;
+		if(!(h.name.trim().equals(this.name.trim())))return false;
+		return true;
+	}
+	public int hashCode() {
+		int r = 13;
+		int rr = 17;
+		r = name.hashCode()*r*rr;
+		return r;
+	}
+}
 public class Main {
     public static void main(String[] args) {
-    	Set<Hero> list = new HashSet<>();
+    	Set<Object> list = new HashSet<>();
     	Hero h1 = new Hero();
-
-    	System.out.println("h1 = "+h1);
-//    	System.out.println(h1.hashCode());
-//11行目は、Heroクラスにhashcode()があるとエラーになる。理由はname.hashcode()としてるので、
-//nameを設定しないと計算ができないからと思われる。
     	h1.setName("TARO");
-    	System.out.println(h1.hashCode());
-    	System.out.println("h1 = "+h1);
-
+    	Monster m = new Monster();
+    	m.setName("TARO");
     	list.add(h1);
-    	h1 = new Hero();
-//    	System.out.println(h1.hashCode());
-    	System.out.println("h1 = "+h1);
-
-    	h1.setName("TARO");
-    	System.out.println(h1.hashCode());
-       	System.out.println("list.size() = "+list.size());
-
-    	list.remove(h1);
-
+    	list.add(m);
     	System.out.println("list.size() = "+list.size());
 
+    	System.out.println("m.hashCode()="+m.hashCode());
+    	System.out.println("h1.hashCode()="+h1.hashCode());
+
+    	Iterator<Object> it = list.iterator();
+    	while(it.hasNext()) {
+    		String name ="";
+//    		String mn;
+
+    		Object e = it.next();
+    		if(e instanceof Hero) {
+    			Hero hr = (Hero)e;
+    			name = hr.getName();
+//    			if(name.equals("TARO")) {
+//    				list.remove(hr);
+//    			}
+    		}
+    		if(e instanceof Monster) {
+    			Monster mr = (Monster)e;
+    			name = mr.getName();
+//    			if(name.equals("TARO")) {
+//    				list.remove(mr);
+//    			}
+    		}
+    		System.out.println("test");
+    		System.out.println(list);
+    	}
+//    	ifがあるとなぜダメなのかhashsetを勉強する必要がある
     }
 }
