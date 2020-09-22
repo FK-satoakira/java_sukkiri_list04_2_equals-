4ef9c5eb683ab99cdb0faabf31fc056adb5db5f2
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-22 17:20:01 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -7,31 +7,27 @@ public class Main {
     public static void main(String[] args) {
     	Set<Hero> list = new HashSet<>();
     	Hero h1 = new Hero();
-    	h1.setName("TARO");
+
     	System.out.println("h1 = "+h1);
-    	System.out.println("");
+//    	System.out.println(h1.hashCode());
+//11行目は、Heroクラスにhashcode()があるとエラーになる。理由はname.hashcode()としてるので、
+//nameを設定しないと計算ができないからと思われる。
+    	h1.setName("TARO");
+    	System.out.println(h1.hashCode());
+    	System.out.println("h1 = "+h1);
+
     	list.add(h1);
-    	System.out.println("list.size() = "+list.size());
-    	System.out.println("");
     	h1 = new Hero();
-    	System.out.println("h1を初期化");
-    	System.out.println("");
+//    	System.out.println(h1.hashCode());
     	System.out.println("h1 = "+h1);
-//    	ここ注意。Hashだとこれはjava.lang.NullPointerExceptionになる。
-//    	toStringをオーバーライドするとOKなので、多分、toStringをオーバーライドしていないからと思われる。
-    	System.out.println("");
 
-    	System.out.println("名前を上書き");
-    	System.out.println("");
     	h1.setName("TARO");
-    	System.out.println("h1 = "+h1);
-//    	System.out.println("list.get(0) = "+list.get(0));
-    	System.out.println("");
-    	System.out.println("removeを使う");
+    	System.out.println(h1.hashCode());
+       	System.out.println("list.size() = "+list.size());
+
     	list.remove(h1);
-    	System.out.println("");
+
     	System.out.println("list.size() = "+list.size());
 
-
-   }
+    }
 }
